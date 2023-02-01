f60d4c69ffff138ee96158aa974b678d5a3c7696
NA <na@NAui-MacBookAir.local>
2023-02-01 13:28:19 +0900

    Add:종 errortype 체크 로직 추가

--- src/Data.js
+++ src/Data.js
@@ -15,14 +15,17 @@ const Data = () => {
       let read_buffer = read(fdata, { type: "binary" });
       read_buffer.SheetNames.forEach(function (sheetName) {
         let rowdata = utils.sheet_to_json(read_buffer.Sheets[sheetName]);
-        const newData = setDefaultData(rowdata);
-        setType(newData);
-        const result = checkDuplicate(newData);
-        sortData(result, false);
+        handleData(rowdata);
       });
     };
     reader.readAsBinaryString(input[0]);
   }
+  const handleData = (data) => {
+    const newData = setDefaultData(data);
+    setType(newData);
+    const result = checkDuplicate(newData);
+    sortData(result, false);
+  };
 
   const formatPhoneNumber = (value) => {
     const firstLength = value.length > 9 ? 3 : 2;
@@ -96,7 +99,7 @@ const Data = () => {
             ? "고양이"
             : item.type.toLowerCase() === "canine"
             ? "개"
-            : "etc";
+            : "기타";
         return item.type;
       }
     });
@@ -111,18 +114,28 @@ const Data = () => {
     return result;
   };
 
+  const removeErrorType = (item, key) => {
+    const index = item.errorType.findIndex((element) => {
+      return element === key;
+    });
+    if (index !== -1) {
+      item.errorType.splice(index, 1);
+    }
+  };
+
   const checkPhoneNumber = (item) => {
     if (!regphone.test(item.phone_number)) {
       if (item.errorType.indexOf("phone_number") === -1) {
         item.errorType.push("phone_number");
       }
     } else {
-      const index = item.errorType.findIndex((element) => {
-        return element === "phone_number";
-      });
-      if (index !== -1) {
-        item.errorType.splice(index, 1);
-      }
+      removeErrorType(item, "phone_number");
+    }
+  };
+
+  const checkType = (item) => {
+    if (item.type === "개" || item.type === "고양이" || item.type === "기타") {
+      removeErrorType(item, "type");
     }
   };
 
@@ -135,13 +148,10 @@ const Data = () => {
       } else {
         if (key === "phone_number") {
           checkPhoneNumber(item);
+        } else if (key === "type") {
+          checkType(item);
         } else {
-          const index = item.errorType.findIndex((element) => {
-            return element === key;
-          });
-          if (index !== -1) {
-            item.errorType.splice(index, 1);
-          }
+          removeErrorType(item, key);
         }
       }
     });
@@ -189,6 +199,7 @@ const Data = () => {
     }));
     setInfoData(transformedData);
     console.log("complete");
+    console.log(transformedData);
   };
 
   return (
